f00289352fbe24e46cd999a5eeeb13957276beed
Danilo Patanè <d.patane98@gmail.com>
2020-08-29 13:15:49 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,62 +1,99 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100
-// i numeri non possono essere duplicati
-// in seguito deve chiedere all'utente 84 volte di inserire un numero alla volta, sempre compreso tra 1 e 100
-// l'utente non può inserire lo stesso numero più volte
-// se il numero è presente nella lista dei numeri generati il gioco finisce se no continua
-// La partita termina quando il giocatore inserisce un numero generato o il numero massimo di numeri consentiti
-// al termine della partita il software deve comunicare il punteggio cioè il numero di volte che l'utente ha inserito un numero consentito
 
-// funzione che genera tra 1 e 100
-
-function genNumeri(num) {
-
-  return Math.floor(Math.random() * num) + 1
-}
-
-
-
-//funzione che il numero non è duplicato
-function duplicatoCheck(elementoDaVerificare, array) {
-  var checkDoppio = false;
-  for (var i = 0; i < array.length; i++) {
-    if (elementoDaVerificare === array[i]) {
-        var checkDoppio = true
-    }
-
-  }
-}
 // FINE FUNZIONI
+function genNumeri(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// funzione che il numero non è duplicato
+
+function duplicatoCheck(numero, array) {
+ if (array.indexOf(numero) >= 0) {
+ return true
+ }
+ else {
+  return false
+}
+}
+function rangeNumero (numero, min, max) {
+if (numero >= min && numero <= max) {
+  return true
+} else {
+  return false;
+}
+}
+
+var bombe = 16;
+var tentativi = 100;
 
 
 
 // creo una variabile array con i numeri di genNUmeri
 var arrayBombe = [];
-var x = 0;
 
 // ciclo che chiama la mia funzione di generazione dei numeri 16 volte
-while (x == 16) {
-  var numeroGenerato = genNumeri(100);
-  console.log(numeroGenerato);
-  // mi chiamo il check della funzione duplicati       // se non è duplicato lo salvo nell'array bombe
-  if (duplicatoCheck(numeroGenerato, arrayBombe) === false) {
-     arrayBombe.push(numeroGenerato);
-  }
+for (var i = 0; i < bombe; i++) {
+  var numeroGenerato;
+  do {
+    numeroGenerato = genNumeri(1, tentativi);
 
+
+  } while (duplicatoCheck(numeroGenerato, arrayBombe) === true);
+    arrayBombe.push(numeroGenerato);
 }
-
-
 console.log(arrayBombe);
 
-
-
 // creo una variabile array che contiene i numeri utente
-
+var numeriUtente = [];
 // variabile booleana haPerso = false
+var haPerso = false;
 
+var ultimoCiclo = false;
+var i = 0;
 // ciclo di 84 volte
-  // prompt in cui chiedo all'utente un numero
-  // check che il numero sia compreso tra 1 e 100
-  // check che il numero non sia duplicato nell'array utente
+while (i < tentativi - bombe && haPerso == false && ultimoCiclo == false) {
+  do {
+    var vaiCiclo = false;
+    var numeroInseritoUtente = parseInt(prompt("inserisci un numero tra 1 e 100 ed evita la bomba"));
+    if (isNaN(numeroInseritoUtente)) {
+      alert ("Inserisci un numero non una parola");
+      vaiCiclo = true;
+    }
+     else if (rangeNumero(numeroInseritoUtente, 1 , tentativi)) {
+       // check che il numero non sia duplicato nell'1array utente
+
+      if (duplicatoCheck(numeroInseritoUtente, numeriUtente)) {
+        alert ("Hai gia inserito questo numero");
+        vaiCiclor = true;
+      }
+      else {
+        vaiCiclo = false;
+      }
+
+    }
+     else {
+      alert ("Devi inserire un numero compreso tra 1 e 100");
+      vaiCiclo = true;
+    }
+  } while (vaiCiclo == true) {
+    if (duplicatoCheck(numeroInseritoUtente, arrayBombe)) {
+      haPerso = true;
+      alert ("hai perso hai beccato una bomba il livello che hai raggiunto è il: " + numeriUtente.length)
+    } else {
+      numeriUtente.push(numeroInseritoUtente);
+      console.log(numeriUtente);
+      if (i == tentativi - bombe - 1) {
+        ultimoCiclo = true;
+      }
+    }
+    i++;
+  }
+}
+
+  if (haPerso == false) {
+    alert ("hai vinto")
+  }
+
+
   // se non è duplicato salvo il numero nell'array utente
   // check che il numero non sia nell'array bomba
   // se il numero è nell'array bomba - allert hai perso ed esco dal ciclo
